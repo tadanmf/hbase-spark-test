af3ff175af2d298f55c93fa76381b4b9aad7c023
tadanmf <tadanmf@gmail.com>
2021-04-23 19:03:11 +0900

    solr 결과값으로 hive 조회

--- src/test/scala/io/datadynamics/solr/HiveSolr.scala
+++ src/test/scala/io/datadynamics/solr/HiveSolr.scala
@@ -1,7 +1,7 @@
 package io.datadynamics.solr
 
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.junit.Test
 import org.slf4j.{Logger, LoggerFactory}
 import com.hortonworks.spark.sql.hive.llap.{HiveWarehouseBuilder, HiveWarehouseSessionImpl}
@@ -52,12 +52,50 @@ class HiveSolr {
 
   @Test
   def selectChatsTest(): Unit = {
+    val sparkConfig: SparkConf = new SparkConf().setMaster("local[2]").setAppName("Solr-Hive Test with Spark")
+    sparkConfig.set("spark.sql.warehouse.dir", "warehouse")
+    val spark: SparkSession = SparkSession.builder().config(sparkConfig).enableHiveSupport().getOrCreate()
+
     // 검색어
-    val userNick = ""
+    val userNick = "토마토살려내"
 
     // get solr result
+    val options = Map(
+      "collection" -> "chat_nick_change",
+      "zkhost" -> "tt05cn001.hdp.local:2181,tt05nn001.hdp.local:2181,tt05nn002.hdp.local:2181/solrtest"
+    )
+    val df: DataFrame = spark.read.format("solr")
+      .options(options)
+      .option("filters", s"userNick:${userNick}")
+      .load
+
+    df.show()
 
     // get hive result
+    spark.conf.set("spark.sql.hive.hiveserver2.jdbc.url", "jdbc:hive2://tt05nn001.hdp.local:2181,tt05cn001.hdp.local:2181,tt05nn002.hdp.local:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2;user=hive;password=hive")
+    val hive: HiveWarehouseSessionImpl = HiveWarehouseBuilder.session(spark).build()
+
+    df.collect().map(row => {
+      //val cf: Long = row.getAs[Long](0) / 1000
+      val startTime: Long = row.getAs[Long]("nickChangeStartTime")
+      val endTime: Long = row.getAs[Long]("nickChangeEndTime")
+      val q: String = row.getAs[String]("userId")
+      //(cf, q)
+      val sqlStr = s"select chat['${q}'], ts from hbase_hive_table3 where chat['${q}'] is not null and (unix_timestamp(ts, 'yyyy-MM-dd HH:mm:ss.SSS') >= unix_timestamp(from_unixtime(${startTime}, 'yyyy-MM-dd HH:mm:ss.SSS'), 'yyyy-MM-dd HH:mm:ss.SSS') and unix_timestamp(ts, 'yyyy-MM-dd HH:mm:ss.SSS') < unix_timestamp(from_unixtime(${endTime}, 'yyyy-MM-dd HH:mm:ss.SSS'), 'yyyy-MM-dd HH:mm:ss.SSS')) LIMIT 5"
+
+      //val resultRow: Dataset[Row] = hive.execute(sqlStr)
+      //resultRow.take(3)foreach(row => {
+      //  logger.info(s"row >> ${row}")
+      //})
+
+      //resultRow.map(row => {
+      //  logger.info(s"row >> ${row}")
+      //  //row.getValuesMap(Seq(s"chat[${q}]"))
+      //  //logger.info(s"[${q}] ${}")
+      //})
+
+      hive.execute(sqlStr).show()
+    })
 
     // output: [nick] chat text
   }
